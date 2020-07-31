2095b1f26f869eb3175c6c442089627c54f32917
Falitac <fastkonrad@gmail.com>
2020-07-31 09:56:21 +0200

    added text info and coloring

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,40 @@
 #include<complex>
 #include<SFML/Graphics.hpp>
 
+// Color conversion HSL -> RGB
+sf::Color HSLtoRGB(float H, float S, float L)
+{
+	float C = (1 - std::fabs(2 * L - 1)) * S; // Chroma
+	float HPrime = H / 60; // H'
+	float X = C * (1 - std::fabs(std::fmod(HPrime, 2.f) - 1));
+	float M = L - C / 2;
+
+	float R = 0.f;
+	float G = 0.f;
+	float B = 0.f;
+
+	switch (static_cast<int>(HPrime))
+	{
+	case 0: R = C; G = X;        break; // [0, 1)
+	case 1: R = X; G = C;        break; // [1, 2)
+	case 2:        G = C; B = X; break; // [2, 3)
+	case 3:        G = X; B = C; break; // [3, 4)
+	case 4: R = X;        B = C; break; // [4, 5)
+	case 5: R = C;        B = X; break; // [5, 6)
+	}
+
+	R += M;
+	G += M;
+	B += M;
+
+	sf::Color color;
+	color.r = static_cast<sf::Uint8>(std::round(R * 255));
+	color.g = static_cast<sf::Uint8>(std::round(G * 255));
+	color.b = static_cast<sf::Uint8>(std::round(B * 255));
+
+	return color;
+}
+
 // Context for easy access
 sf::RenderWindow* globWin;
 
@@ -68,9 +102,10 @@ void updateCnv(
 					);
 		int m = mandelbrot(std::complex<double>( {mappedX, mappedY} ), max_iter);
 		//m = 255*std::log2(m)/std::log2(max_iter);
-		m = 255*m/max_iter;
+		//m = 255*m/max_iter;
+		auto pixelColor = HSLtoRGB(360 * std::log2(m)/std::log2(max_iter), 0.7f, 0.6f);  
 
-		cnv.setPixel(x, y, sf::Color(255, 255, 255, m));
+		cnv.setPixel(x, y, pixelColor);
 	}
 }
 
